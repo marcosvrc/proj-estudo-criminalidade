ae12a3aad5964f72a7b279c7aad0942706e95048
Marcos Camara <marcosramosvc@gmail.com>
2019-11-03 09:32:03 -0300

    Alteração Código

--- src/main/java/br/com/proj/estudo/criminalidade/ProjEstudoCriminalidadeMapper.java
+++ src/main/java/br/com/proj/estudo/criminalidade/ProjEstudoCriminalidadeMapper.java
@@ -7,25 +7,29 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-
 public class ProjEstudoCriminalidadeMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-	
+
 	private static final IntWritable ONE = new IntWritable(1);
-	
-	
+	private static final int QTDE_COLUNAS = 21;
+
 	@Override
-	public void map(LongWritable key, Text value, Context context)  throws IOException, InterruptedException{
-		
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
 		String coluna = value.toString();
-		String[] linhaRegistroBO = coluna.split(";");
-		//Cidade que ocorreu o BO
-		context.write(new Text(linhaRegistroBO[17]), ONE);
-		
-		//Mês que ocorreu o BO
-		//context.write(new Text(linhaRegistroBO[10]), ONE);
-		
-		//Motivo do Boletim de Ocorrência
-		//context.write(new Text(linhaRegistroBO[12]), ONE);
+		String[] linhaRegistroBO = coluna.split(",");
+
+		if (linhaRegistroBO.length == QTDE_COLUNAS) {
+
+			// Cidade que ocorreu o BO
+			context.write(new Text(linhaRegistroBO[17]), ONE);
+
+			// Mês que ocorreu o BO
+			//context.write(new Text(linhaRegistroBO[10]), ONE);
+
+			// Motivo do Boletim de Ocorrência
+			//context.write(new Text(linhaRegistroBO[12]), ONE);
+		}
+
 	}
 
 }
